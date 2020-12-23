938da4552d4fd3005fedc0e6aadc64182c847654
c2909371614 <2909371614@qq.com>
2020-12-23 09:04:56 +0800

    update code

--- RelicSecAttAnalysis.py
+++ RelicSecAttAnalysis.py
@@ -8,19 +8,23 @@ import numpy as np
 import pandas as pd
 from sklearn.linear_model import LinearRegression
 
-RelicsData = pd.read_excel('圣遗物副属性.xlsx')
+
+RelicsData = pd.read_excel('原神数据.xlsx', sheet_name = '圣遗物副属性')
 RelicsData.fillna(0, inplace=True )
-features = ['ATK', 'ATK_1', 'CR', 'CD', 'EM', 'EE', 'HP', 'HP_1', 'DEF', 'DEF_1']
+# write = pd.ExcelWriter()#不会清空其他sheet
+# RelicsData.to_excel('test.xlsx', sheet_name = 'sheet2', index = True)
+features = ['ATK', 'ATK_1', 'CR', 'CD', 'EM', 'ER', 'HP', 'HP_1', 'DEF', 'DEF_1']
 # print(features[:10])
 X_train = RelicsData[features]
 len_X = len(X_train)
-Y_train = np.ones((len_X,), dtype = np.int)*4
+Y_train = np.ones((len_X,), dtype = np.int)*9
 # X_train.info()
 linreg = LinearRegression(fit_intercept = False)#将截距设置为0
 linreg.fit(X_train,Y_train)
 y_predict = linreg.predict(X_train)
 print(linreg.score(X_train, Y_train))
-print(linreg.coef_, '截距:%.3f' %linreg.intercept_)
+print(linreg.coef_)
+print('截距:%.3f' %linreg.intercept_)
 # print(linreg.get_params())
 # RelicsData.info()
 # print('ok')
--- simpleCalcE_S.py
+++ simpleCalcE_S.py
@@ -2,9 +2,23 @@
 #@author: cpx
 #creating time = 2020.12.13
 
-ATK = float(input('输入攻击力：'))
-CR = float(input('输入暴击率：'))
-CD = float(input('输入暴击伤害：'))
-EDA = float(input('输入伤害加成：'))
-E_S = ATK*(1+CR*CD)*(1+EDA)
+CR = CD = DA = ATK = 0
+def Get_Input():
+	ATK = float(input('输入攻击力：'))
+	CR = float(input('输入暴击率：'))
+	CD = float(input('输入暴击伤害：'))
+	DA = float(input('输入伤害加成：'))
+	return [CR,CD,DA,ATK]
+def Get_Txt():
+	with open('原神角色数值/七七面板.txt', 'r') as file:
+		ATK = float(file.readline())
+		CR = float(file.readline())
+		CD = float(file.readline())
+		DA = float(file.readline())
+	return [CR,CD,DA,ATK]
+	# print(CR,CD,DA,ATK)
+CR,CD,DA,ATK = Get_Txt()
+print('ATK:%.0f\nCR:%.3f\nCD:%.3f\nDA:%.3f'%(ATK,CR,CD,DA))
+E_S = ATK*(1+CR*CD)*(1+DA)
+
 print('预期伤害：%.2f' %E_S)
